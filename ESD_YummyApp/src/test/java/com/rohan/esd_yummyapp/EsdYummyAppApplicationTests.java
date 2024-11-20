297df7db5b27d035e9c8318309eded7a55295dbc
Rohan Sonawane <rohansonawane1818@gmail.com>
2024-11-20 22:30:39 +0530

    Jwt authorization completed

---
+++ ESD_YummyApp/src/test/java/com/rohan/esd_yummyapp/EsdYummyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.esd_yummyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdYummyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
